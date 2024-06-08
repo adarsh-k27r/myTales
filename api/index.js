3e0c37e8f9955039ba0e8efc2bb755bde451ee43
Adarsh Kumar <21cs3060@rgipt.ac.in>
2024-06-08 23:10:03 +0530

    Dev Env is ready with basic routing

---
+++ api/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("MongoDB connected succesfully!");
+}).catch(err=>{
+    console.log(err);
+})
+
+app.listen(3000, ()=>{
+    console.log("Server listening on port 3000!")
+})
+
+
+
