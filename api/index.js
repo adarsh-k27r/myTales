0b7fbfc375ec658cd4cb41941027be49767d16e3
Adarsh Kumar <21cs3060@rgipt.ac.in>
2024-07-04 16:51:27 +0530

    Publish page UI made and create post api

--- api/index.js
+++ api/index.js
@@ -1,43 +1,41 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRoute from './routes/user.route.js'
-import authRoute from './routes/auth.route.js'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("MongoDB connected succesfully!");
-}).catch(err=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-
-app.listen(3000, ()=>{
-    console.log("Server listening on port 3000!")
-})
+app.listen(3000, () => {
+  console.log("Server listening on port 3000!");
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/post", postRoutes);
 
-app.use((err, req, res, next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message
-    });
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
-
-
-
-
-
--- client/src/pages/Publish.jsx
+++ client/src/pages/Publish.jsx
@@ -1,22 +1,55 @@
-import React from "react";
-import { Button, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Alert, Button, TextInput } from "flowbite-react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useNavigate } from "react-router-dom";
 
 const Publish = () => {
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/post/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+      if (res.ok) {
+        setPublishError(null);
+        navigate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError("Something went wrong");
+    }
+  };
+
   return (
     <>
       <div className="p-3 max-w-3xl mx-auto">
         <h1 className="text-center text-3xl my-3 font-semibold">
           Share your story
         </h1>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <TextInput
             type="text"
             placeholder="Title"
             required
             id="title"
             className="flex-1"
+            onChange={(e) => {
+              setFormData({ ...formData, title: e.target.value });
+            }}
           />
 
           <ReactQuill
@@ -24,11 +57,19 @@ const Publish = () => {
             placeholder="It's confession time:> Just do it!"
             className="h-72 mb-12"
             required
+            onChange={(value) => {
+              setFormData({ ...formData, content: value });
+            }}
           />
 
           <Button type="submit" gradientDuoTone="redToYellow">
             Publish
           </Button>
+          {publishError && (
+            <Alert className="mt-5" color="failure">
+              {publishError}
+            </Alert>
+          )}
         </form>
       </div>
     </>
